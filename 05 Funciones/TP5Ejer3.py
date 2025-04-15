087a55be44b1e80e2e331ed4725b7886b855ab8d
gabito00 <gabrielm.montana@gmail.com>
2025-04-15 20:32:19 -0300

    Ejercicio 3

---
+++ 05 Funciones/TP5Ejer3.py
@@ -0,0 +1,17 @@
+# Crear una función llamada informacion_personal(nombre, apellido,
+# edad, residencia) que reciba cuatro parámetros e imprima: “Soy
+# [nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pedir los datos al usuario y llamar a esta función con los valores ingresados.
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+
+
+def main():
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    edad = input("Ingrese su edad: ")
+    residencia = input("Ingrese su residencia: ")
+    informacion_personal(nombre, apellido, edad, residencia)
+
+if __name__ == "__main__":
+    main()
