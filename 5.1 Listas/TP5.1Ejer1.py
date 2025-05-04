7113b7a1740c75ae8e0edae84bbbdbe01892450f
gabito00 <gabrielm.montana@gmail.com>
2025-05-04 19:39:13 -0300

    ejer 1 al 10 agregados

---
+++ 5.1 Listas/TP5.1Ejer1.py
@@ -0,0 +1,5 @@
+# 1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función
+# range.
+
+miLista = list(range(4,101,4))
+print(miLista)
---
+++ 5.1 Listas/TP5.1Ejer5.py
@@ -0,0 +1,8 @@
+# 5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
+# numeros = [0, 15, 3, 22, 7]
+# numeros.remove(max(numeros))
+# print(numeros)
+primerPaso= "Crea una lista con los numeros 0, 15, 3, 22 y 7"
+segundoPaso = "Identifica el maximo numero en la lista"
+tercerPaso = "Elimina el maximo valor de la lista con la funcion remove, en este caso seria numeros.remove(22)"
+print(primerPaso + "\n" + segundoPaso + "\n" + tercerPaso)
